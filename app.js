c2618c8be548cac6d60e6005e9767fc79b2f8dce
Vibhor Bakshi <vibhor.bakshi1998@gmail.com>
2018-10-10 15:24:16 +0530

    Added server and client side validation and body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 
 const app=express();
@@ -14,6 +15,9 @@ const PORT=3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
   res.render('index',{
@@ -33,10 +37,24 @@ app.get('/notes/add',(req,res)=>{
   });
 });
 
-app.get('/notes',(req,res)=>{
-  res.render('notes',{
-    title:'Your Notes'
-  });
+app.post('/notes',(req,res)=>{
+  let err=[];
+  if(!req.body.title_form){
+  err.push({text:'Please enter the title'});
+  }
+  if(!req.body.details){
+  err.push({text:'Please enter the deatails'});
+  }
+  if(err.length>0){
+    res.render('notes/add',{
+      errors:err,
+      title_form:req.body.title_form,
+      details:req.body.details
+    });
+  }
+  else{
+    res.send('Passed');
+  }
 });
 
 app.listen(PORT,()=>{
