f2c493441c218d92972cf3ab2c9a76a6e77da3c8
Vibhor Bakshi <vibhor.bakshi1998@gmail.com>
2018-10-12 18:17:51 +0530

    Added login functionality using passport and hashed password using bcrypt

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 var mongoose=require('mongoose');
 
 const app=express();
@@ -12,6 +13,7 @@ const app=express();
 const notes=require('./routes/notes');
 const users=require('./routes/users');
 
+require('./config/passport')(passport);
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/notes-dev',{ useNewUrlParser: true })
@@ -32,6 +34,10 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
  app.use(flash());
 
  // Global Variables
@@ -39,6 +45,7 @@ app.use(session({
    res.locals.success_msg=req.flash('success_msg');
    res.locals.error_msg=req.flash('error_msg');
    res.locals.error=req.flash('error');
+   res.locals.user=req.user||null;
    next();
  });
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router=express.Router();
+const bcrypt=require('bcryptjs');
+const passport = require('passport');
 const {Note,User}=require('../models/User');
 
 
@@ -9,6 +11,14 @@ router.get('/login',(req,res)=>{
 
 router.get('/register',(req,res)=>{
   res.render('users/register');
+});
+
+router.post('/login',(req,res,next)=>{
+  passport.authenticate('local',{
+    successRedirect:'/notes',
+    failureRedirect:'/users/login',
+    failureFlash:true
+  })(req,res,next);
 });
 
 router.post('/register',(req,res)=>{
@@ -26,7 +36,34 @@ router.post('/register',(req,res)=>{
       email:req.body.email
     });
   }else {
-    res.send('Passed');
+    User.findOne({email:req.body.email})
+      .then(user=>{
+        if (user) {
+          req.flash('error_msg','Email is already registered');
+          res.redirect('/users/login');
+        }else {
+          const newUser=new User({
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password
+          });
+          bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(newUser.password,salt,(err,hash)=>{
+              if(err) throw err;
+              newUser.password=hash;
+              newUser.save()
+                .then(user=>{
+                  req.flash('success_msg','You are now registered and can now login');
+                  res.redirect('/users/login');
+                })
+                .catch((err) => {
+                  console.log(err);
+                  return;
+                });
+            });
+          });
+        }
+      });
   }
 });
 
